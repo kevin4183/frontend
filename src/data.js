11d9bd50149aea35c6ea339f368ca8511a2ddc45
kevin4183 <anwannandikenneth@gmail.com>
2022-11-28 13:38:38 +0100

    added second change

---
+++ src/data.js
@@ -0,0 +1,67 @@
+import { IAdidas, IAdidas1, INike, INike1 } from './icons';
+
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: IAdidas, // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: IAdidas1,
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: INike,
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: INike1,
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas okFit Pant',
+      slug: 'adidas-okfit-pant',
+      category: 'Pantsonwheels',
+      image: INike1,
+      price: 65,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4,
+      numReviews: 100,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
---
+++ src/icons.js
@@ -0,0 +1,9 @@
+import Adidas from '../public/images/Adidas.jpg';
+import Adidas1 from '../public/images/Adidas1.jpg';
+import Nike from '../public/images/Nike.jpg';
+import Nike1 from '../public/images/Nike1.jpg';
+
+export const IAdidas = Adidas;
+export const IAdidas1 = Adidas1;
+export const INike = Nike;
+export const INike1 = Nike1;
